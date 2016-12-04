50eb11bc3e3950c1b7f3d05b780b29cba9178040
Sean Chen <sean820816@yahoo.com.tw>
2016-12-04 11:18:18 +0800

    demo ok

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var shouldPlay = false;
 
-
-
 $(document).ready(function() {
 
 	// $('div').animate({ top: '-=200px' }, 600, function () {  })
@@ -40,6 +38,19 @@ function stopPlayButton() {
         $("#record_btn_image").attr('src', "design/image/result_play.png");
         $("#subtitle").html('我們想讓你知道，\n你不是獨自在面對這一切')
         $("#a").css('display', 'none');
+
+        var old_element = document.getElementById("record_btn");
+				var new_element = old_element.cloneNode(true);
+				old_element.parentNode.replaceChild(new_element, old_element);
+        $("#record_btn").click(function() {
+        	var audio = document.getElementById("record_feedback");
+				  audio.play();
+				  $("#record_btn_image").attr('src', "design/image/result_stop.png");
+
+				  setTimeout(function() {
+				  	window.location = "1.html";
+				  }, 10000);
+        });
     }, 4000)
 
 }
