6daf6c36bc0ea1ffd933fcae274d10d6c66732fd
Wilson Li <b84330808@gmail.com>
2016-12-04 10:20:15 +0800

    css

--- js/index.js
+++ js/index.js
@@ -1,34 +1,50 @@
 var shouldPlay = false;
 
+
+
 $(document).ready(function() {
 
-	$('#record_btn').click(function(event){
-		// Change record button's image
-		if (!shouldPlay) {
-			$("#record_btn_image").attr('src', "design/image/recording_stop.png");	
-		} else {
-			$("#record_btn_image").attr('src', "design/image/recording_start.png");	
-		}
+	// $('div').animate({ top: '-=200px' }, 600, function () {  })
 
-		// Change image to iat result text
-		if (!shouldPlay) {
-			$('#image_container').css('display', 'none');	
-		} else {
-			$('#image_container').css('display', '');	
-		}
 
-		shouldPlay = !shouldPlay;
-	});
+    $('#record_btn').click(function(event) {
+        // Change record button's image
+        if (!shouldPlay) {
+            $("#record_btn_image").attr('src', "design/image/recording_stop.png");
+        } else {
+            $("#record_btn_image").attr('src', "design/image/recording_start.png");
+        }
+
+        // Change image to iat result text
+        if (!shouldPlay) {
+            $('#image_container').css('display', 'none');
+        } else {
+            $('#image_container').css('display', '');
+        }
+
+        shouldPlay = !shouldPlay;
+    });
 });
 
 function stopPlayButton() {
-	$("#record_btn_image").attr('src', "design/image/recording_start.png");
-	// $('#image_container').css('display', '');	
-	// $('#iat_result_container').css('display', 'none');
-	shouldPlay = false;
+    $("#record_btn_image").attr('src', "design/image/recording_start.png");
+    // $('#image_container').css('display', '');	
+    // $('#iat_result_container').css('display', 'none');
+    shouldPlay = false;
+    // --------------------------
+
+    setTimeout(function() {
+        $("#iat_result").remove();
+        $('#image_container').css('display', '');
+        $("#big-img").attr('src', "design/image/result_play_hand.png");
+        $("#record_btn_image").attr('src', "design/image/result_play.png");
+        $("#subtitle").html('我們想讓你知道，\n你不是獨自在面對這一切')
+        $("#a").css('display', 'none');
+    }, 3000)
+
 }
 
 function addQuote(str) {
-	var newStr = "「" + str + "」";
-	return newStr;
+    var newStr = "「" + str + "」";
+    return newStr;
 }
